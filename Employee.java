670810bded94ce1a858c6af112ef761683346978
codesDipti123 <np03cs4s240064@heraldcollege.edu.np>
2025-01-11 22:22:31 +0545

    Updated Employee.java with new functionality by adding getters, setters methods

--- Employee.java
+++ Employee.java
@@ -12,6 +12,39 @@ public class Employee {
         this.salary = salary;
     }
 
+        // Getters and Setters
+        public int getId() {
+            return id;
+        }
+    
+        public void setId(int id) {
+            this.id = id;
+        }
+    
+        public String getName() {
+            return name;
+        }
+    
+        public void setName(String name) {
+            this.name = name;
+        }
+    
+        public String getPosition() {
+            return position;
+        }
+    
+        public void setPosition(String position) {
+            this.position = position;
+        }
+    
+        public double getSalary() {
+            return salary;
+        }
+    
+        public void setSalary(double salary) {
+            this.salary = salary;
+        }
+    
     // toString method
     @Override
     public String toString() {
